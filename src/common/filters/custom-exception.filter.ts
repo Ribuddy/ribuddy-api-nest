6429c17f06b7debe83899250c4b84caadffa9c52
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-10-01 05:19:35 +0900

    ♻️ Refactor: SRP에 따라 Exception별 Filter 구분
    여러 문제로 인해 다시 하나의 GlobalFilter로 합칠 예정

--- src/common/filters/custom-exception.filter.ts
+++ src/common/filters/custom-exception.filter.ts
@@ -1,31 +1,25 @@
-import {
-  ArgumentsHost,
-  Catch,
-  ExceptionFilter,
-  HttpException,
-  Inject,
-  LoggerService,
-} from '@nestjs/common';
+import { ArgumentsHost, Catch, ExceptionFilter, Inject, LoggerService } from '@nestjs/common';
 
 import { Request, Response } from 'express';
 import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
 
+import { CustomException } from '@common/codes/custom.exception';
 import { ApiCommonResponse } from '@common/dto/common-response.dto';
 import {
   parseContextToRequestInfo,
-  parseHttpExceptionToErrorDetails,
+  parseCustomExceptionToErrorDetails,
 } from '@common/utils/parse-http-exception.util';
 
-@Catch(HttpException)
-export class HttpExceptionFilter implements ExceptionFilter {
+@Catch(CustomException)
+export class CustomExceptionFilter implements ExceptionFilter {
   constructor(@Inject(WINSTON_MODULE_NEST_PROVIDER) private readonly logger: LoggerService) {}
 
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: CustomException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const request = ctx.getRequest<Request>();
     const response = ctx.getResponse<Response>();
 
-    const errorDetails = parseHttpExceptionToErrorDetails(exception);
+    const errorDetails = parseCustomExceptionToErrorDetails(exception);
 
     const logPayload = {
       // timestamp와 trqceId는 Logger에서 추가됨
@@ -35,13 +29,20 @@ export class HttpExceptionFilter implements ExceptionFilter {
       request: parseContextToRequestInfo(ctx),
     };
 
+    //   this.logger.log(
+    //     LOG_LEVELS.VERBOSE,
+    //     `[${request.method} ${request.url} ${status}]\nStack Trace: ${exception.stack}`,
+    // );
+    // this.logger.log(LOG_LEVELS.VERBOSE, logPayload);
     this.logger.log(logPayload);
 
     // 응답이 전송되지 않은 경우에만 Response 전송
     if (!response.headersSent) {
       response
         .status(errorDetails.httpStatusCode)
-        .json(ApiCommonResponse.fail(errorDetails.code, errorDetails.message, errorDetails.data));
+        .json(
+          ApiCommonResponse.fail(errorDetails.code, '요청이 실패하였습니다.', errorDetails.message),
+        );
     }
   }
 }
