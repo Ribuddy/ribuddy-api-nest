d83ae14f4481083fc602395cd3fb8c2f5019b4e9
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-10-01 05:20:50 +0900

    ✨ Feat: Exception Type에 따라서 parser 함수 분리

--- src/common/utils/parse-http-exception.util.ts
+++ src/common/utils/parse-http-exception.util.ts
@@ -3,45 +3,63 @@ import { HttpArgumentsHost } from '@nestjs/common/interfaces';
 
 import { Request } from 'express';
 
+import { CustomExceptionResponse } from '@common/codes/code.type';
+import { CommonCode } from '@common/codes/common.code';
+import { CustomException } from '@common/codes/custom.exception';
+
+/**
+ * CustomException의 정보를 파싱합니다.
+ * @param exception
+ */
+export const parseCustomExceptionToErrorDetails = (exception: CustomException) => {
+  const errorResponse = exception.getResponse() as CustomExceptionResponse;
+  const httpStatusCode = exception.getStatus();
+
+  return {
+    name: exception.constructor.name,
+    httpStatusCode,
+    code: errorResponse.code,
+    stack: exception.stack ?? undefined,
+    message: errorResponse.message,
+  };
+};
+
+/**
+ * HttpException의 정보를 파싱합니다.
+ * @param exception
+ */
 export const parseHttpExceptionToErrorDetails = (exception: HttpException) => {
   const errorResponse = exception.getResponse();
   const httpStatusCode = exception.getStatus();
-  const errorCode = (errorResponse as any).errorCode ?? 'UNKNOWN_ERROR';
-  const errorMessage = extractErrorDetails(errorResponse);
 
-  const errorDetails = {
+  // ErrorCode와 Message는 고정
+  // 디버깅 시에는 data에 있는 errorResponse를 확인해서, 왜 우리가 의도하지 않은 HttpException이 발생했는지 확인
+  const { code: errorCode, message: errorMessage } = CommonCode.UNKNOWN_HTTP_EXCEPTION;
+
+  console.log(errorResponse, 'errorResponse');
+
+  return {
     name: exception.constructor.name,
     httpStatusCode,
     code: errorCode,
     stack: exception.stack ?? undefined,
     message: errorMessage,
+    data: errorResponse,
   };
-
-  return errorDetails;
 };
 
-const extractErrorDetails = (errorResponse: string | object): string | object => {
-  // 1. errorResponse가 객체이고, 내부에 message 속성이 있는 경우
-  if (typeof errorResponse === 'object' && errorResponse !== null && 'message' in errorResponse) {
-    return (errorResponse as { message: string | object }).message;
-  }
-
-  // 2. 그 외의 경우 (문자열이거나, message 속성이 없는 객체)는 그대로 반환
-  // 이 경우, errorResponse 자체가 중요한 정보입니다.
-  return errorResponse;
-};
-
+/**
+ * Request의 정보를 파싱합니다.
+ * @param ctx
+ */
 export const parseContextToRequestInfo = (ctx: HttpArgumentsHost) => {
   const request = ctx.getRequest<Request>();
 
-  // 3. 요청 정보 상세화
-  const requestInfo = {
+  return {
     method: request.method,
     url: request.originalUrl,
     ip: request.ip,
     userAgent: request.headers['user-agent'],
     referer: request.headers['referer'] || request.headers['referrer'],
   };
-
-  return requestInfo;
 };
