572dfbf5d6750dc156df02538ec7042195d139e5
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-10-04 00:03:20 +0900

    🗑️ Clean: 불용 코드 제거

--- src/modules/users/controllers/v1/users.v1.controller.ts
+++ src/modules/users/controllers/v1/users.v1.controller.ts
@@ -3,7 +3,6 @@ import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
 // 임시 유저 식별 (JWT 붙기 전까지 mock 헤더 사용)
 
-import { GetMyProfileResponseDto } from '@modules/users/dto/user.dto';
 import { UsersService } from '@modules/users/services/users.service';
 
 @ApiTags('users')
@@ -12,7 +11,6 @@ export class UsersV1Controller {
   constructor(private readonly usersService: UsersService) {}
 
   @ApiOperation({ summary: '[WIP] 내 정보 조회' })
-  @ApiOkResponse({ type: GetMyProfileResponseDto })
   @Get('me')
   getUserInfo(@Req() { userId }) {
     // return this.usersService.getUserInfo(userId);
--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -22,46 +22,11 @@ export class User {
   nickname!: string;
 
   @IsString()
-  @ApiProperty({
-    description: '사용자 성별',
-  })
-  gender!: string;
-
-  @IsDate()
-  @Type(() => Date)
-  @IsNotEmpty()
-  @ApiProperty({
-    description: '사용자 생년월일',
-  })
-  birthdate!: Date;
-
-  @IsString()
   @IsOptional()
   @ApiProperty({
     description: '사용자 프로필 이미지',
   })
   profileImage?: string;
-
-  @IsString()
-  @IsOptional()
-  @ApiProperty({
-    description: '사용자 전화번호',
-  })
-  phoneNumber?: string;
-
-  @IsString()
-  @IsOptional()
-  @ApiProperty({
-    description: 'OAuth Provider (e.g., google, facebook)',
-  })
-  oauthProvider?: string;
-
-  @IsString()
-  @IsOptional()
-  @ApiProperty({
-    description: 'OAuth Provider ID',
-  })
-  oauthId?: string;
 
   @IsOptional()
   @Type(() => Date)
@@ -77,37 +42,3 @@ export class User {
   })
   updatedAt!: Date;
 }
-
-export class CreateUserRequestDto extends PickType(User, [
-  'name',
-  'nickname',
-  'gender',
-  'birthdate',
-  'profileImage',
-  'phoneNumber',
-]) {}
-
-export class CreateOAuthUserRequestDto extends User {
-  // TODO: RegisterToken 이용하는 방식이면 필요 없음
-  // oauthProvider!: string;
-  //
-  // oauthId!: string;
-}
-
-export class GetMyProfileResponseDto extends PickType(User, []) {}
-
-export class UpdateNicknameRequestDto extends PickType(User, ['nickname']) {}
-
-export class UpdateProfileImageRequestDto {
-  @ApiProperty({
-    description: '새 프로필 이미지 URL. null이면 삭제로 처리',
-    required: false,
-    nullable: true,
-  })
-  @Transform(({ value }) => (value === null ? null : value))
-  @IsOptional() // undefined는 검증 제외
-  @IsString()
-  @IsUrl({}, { message: '유효한 URL이어야 합니다.' })
-  profileImage?: string | null;
-}
-export class UpdateProfileImageResponseDto {}
