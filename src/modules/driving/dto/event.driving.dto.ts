d4da795bf68ac742a5236db07a4fc75d91372764
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-10-11 15:44:21 +0900

    ✨ Feat: GPX 호환성을 위한 선제 작업 및 요청 형식 정의

--- src/modules/driving/dto/event.driving.dto.ts
+++ src/modules/driving/dto/event.driving.dto.ts
@@ -1,9 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 import { Transform, Type } from 'class-transformer';
-import { IsNotEmpty, ValidateNested } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString, ValidateNested } from 'class-validator';
 
-import { Position } from '@common/dto/position.common.dto';
+import { GpxTrackPoint } from '@modules/driving/dto/common.driving.dto';
 
 export enum DrivingEventType {
   SUDDEN_ACCELERATION = 'SUDDEN_ACCELERATION',
@@ -13,36 +13,52 @@ export enum DrivingEventType {
   ACCIDENT = 'ACCIDENT',
 }
 
-export class CommonEventDetails {
+export class CommonEventDto {
   @ApiProperty({
     description: '이벤트 발생 위치 정보 배열, 각 위치는 latitude와 longitude를 포함합니다.',
+    type: [GpxTrackPoint],
   })
-  @Transform(({ value }) =>
-    value.map((pos: any) => ({
-      latitude: parseFloat(pos.latitude),
-      longitude: parseFloat(pos.longitude),
-    })),
-  )
+  @Transform(({ value }) => value.map((item: any) => Object.assign(new GpxTrackPoint(), item)))
   @IsNotEmpty({ each: true })
   @ValidateNested({ each: true })
-  @Type(() => Position)
-  location!: Position[];
+  @Type(() => GpxTrackPoint)
+  trackSegments!: GpxTrackPoint[];
 
   @IsNotEmpty()
+  @ApiProperty({ enum: DrivingEventType, description: '발생한 이벤트의 유형' })
   type!: DrivingEventType;
+
+  @IsString()
+  @IsOptional()
+  @ApiProperty({
+    example: '급가속(급정거) 이벤트 발생',
+    description: '발생한 이벤트에 대한 간단한 설명. 필수는 아닙니다.',
+  })
+  description?: string;
 }
 
-export class SuddenSpeedChangeEventDetails extends CommonEventDetails {
-  declare type: DrivingEventType.SUDDEN_ACCELERATION;
+export class SuddenSpeedChangeEventDto extends CommonEventDto {
+  declare type: DrivingEventType.SUDDEN_ACCELERATION | DrivingEventType.SUDDEN_STOP;
 
-  @ApiProperty({ example: 3.5, description: '급가속/급정거 시의 가속도 (m/s²)' })
+  @ApiProperty({ example: 1.7, description: '급가속/급정거 시의 가속도 (G 단위)' })
   @IsNotEmpty()
   @Transform(({ value }) => parseFloat(value))
   g_force!: number;
 
   @ApiProperty({ example: 20, description: '급가속/급정거 전 차량 속도 (km/h)' })
-  beforeSpeed!: number;
+  @IsOptional()
+  beforeSpeed?: number;
 
   @ApiProperty({ example: 50, description: '급가속/급정거 후 차량 속도 (km/h)' })
-  afterSpeed!: number;
+  @IsOptional()
+  afterSpeed?: number;
+}
+
+export class AccidentEventDto extends CommonEventDto {
+  declare type: DrivingEventType.ACCIDENT;
+
+  @ApiProperty({ example: 3.5, description: '사고 시의 충격량 (G 단위)' })
+  @IsNotEmpty()
+  @Transform(({ value }) => parseFloat(value))
+  g_force!: number;
 }
