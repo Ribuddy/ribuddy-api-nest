8a47a0b255f8dadad77aaa839865882e3cb9cf01
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-10-12 02:03:26 +0900

    ✨ Feat: google login v2 추가

--- src/modules/auth/services/oauth.auth.service.ts
+++ src/modules/auth/services/oauth.auth.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService, ConfigType } from '@nestjs/config';
 
-import { OAuth2Client } from 'google-auth-library';
-
 import { GOOGLE_OAUTH_CONFIG, GoogleOAuthConfig } from '@modules/auth/config/google-oauth-config';
 import { TokenAuthService } from '@modules/auth/services/token.auth.service';
 import { OAuthUserService } from '@modules/users/services/oauth.users.service';
-import { GoogleOAuthUserData } from '@modules/users/types/oauth.users.types';
 
 @Injectable()
 export class OAuthAuthService {
@@ -19,16 +16,5 @@ export class OAuthAuthService {
   ) {
     this.googleConfig =
       this.configService.getOrThrow<ConfigType<typeof GoogleOAuthConfig>>(GOOGLE_OAUTH_CONFIG);
-  }
-
-  async googleOAuthLoginOrRegister(googleData: GoogleOAuthUserData) {
-    const user = await this.oauthUserService.getUserByOAuth(googleData);
-
-    if (!user) {
-      const newUserId = await this.oauthUserService.createOAuthUserWithGoogle(googleData);
-      return this.tokenAuthService.generateTokens(newUserId);
-    } else {
-      return this.tokenAuthService.generateTokens(user.userId);
-    }
   }
 }
